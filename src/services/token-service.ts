a568118fec91e3e3118bb57a57b6a1b6e3b70278
Nikita Shevchenko <nrcnekita9@gmail.com>
2023-04-10 15:22:27 +0200

    chang expire time for tokens

--- src/services/token-service.ts
+++ src/services/token-service.ts
@@ -3,8 +3,8 @@ import Token from '../schemas/Token';
 
 class TokenService {
   generateTokens(payload: { id: string; email: string }) {
-    const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET!, { expiresIn: '15s' });
-    const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET!, { expiresIn: '30s' });
+    const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET!, { expiresIn: '1h' });
+    const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET!, { expiresIn: '30d' });
     return {
       accessToken,
       refreshToken,
