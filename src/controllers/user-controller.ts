bb47ed6d603bcd3e91d32caa19dd2f1f193c71f2
Nikita Shevchenko <nrcnekita9@gmail.com>
2023-04-18 14:42:27 +0200

    fix: coockie

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -9,9 +9,11 @@ import tokenService from '../services/token-service';
 const cockieSetup = {
   maxAge: 30 * 24 * 60 * 60 * 1000,
   httpOnly: true,
-  domain: process.env.NODE_ENV === 'production' ? process.env.PROD_DOMAIN : process.env.DEV_DOMAIN,
+  // domain: process.env.NODE_ENV === 'production' ? process.env.PROD_DOMAIN : process.env.DEV_DOMAIN,
   secure: true,
+  sameSite: 'none',
 } as CookieOptions;
+console.log(process.env.DEV_DOMAIN);
 
 class UserController {
   async registration(req: Request, res: Response) {
